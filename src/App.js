08724daefc38cfdd2d1a1af273ef99e13a972bcd
raghvis <raghvisaxena@gmail.com>
2020-07-12 19:23:54 +0530

    markers and popups

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
-import { Map, TileLayer} from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl:require('leaflet/dist/images/marker-shadow.png')
+});
+
+const location= [	22.719568, 75.857727];
+const zoom= 12;
 
 function App() {
   return (
     <div className="App">
      <Map center={[0, 0]} zoom={4}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors"></TileLayer>
+      <Marker position={location}>
+      <Popup>
+        Hey Stalker!
+      </Popup>
+      </Marker>
      </Map>
     </div>
   );
